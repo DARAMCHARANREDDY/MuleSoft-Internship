aa3a39f30f5e119f9694b923fb7287f229722e25
DARAMCHARANREDDY <71999946+DARAMCHARANREDDY@users.noreply.github.com>
2021-11-04 22:46:47 +0530

    Update RetrievingData.java

--- RetrievingData.java
+++ RetrievingData.java
@@ -10,14 +10,16 @@ public class RetrievingData
 		
 		Class.forName("org.sqlite.JDBC");
 		Connection con=DriverManager.getConnection("jdbc:sqlite:product:db");
-		String s=sc.next();
-                    String sq="select moviename from movie where leadactor == ?";
-                    PreparedStatement preparedStatement=con.prepareStatement(sq);
-                    preparedStatement.setString(1,s);
-                    ResultSet rset = preparedStatement.executeQuery();
-                    while(rset.next())
-                    {                    System.out.println(rset.getString("moviename"));
-                    }}
+		String sql="select * from movie";
+		Statement ss=con.createStatement();
+		ResultSet rs=ss.executeQuery(sql);
+		System.out.println("Id		MovieName		LeadActor		Actress			Year			Director");
+		System.out.println("--		---------		---------		-------			-----			---------");
+		while(rs.next())
+		{
+			System.out.println(rs.getInt(1)+"		"+rs.getString(2)+"			"+rs.getString(3)+"			"+rs.getString(4)+"			"+
+		rs.getInt(5)+"			"+rs.getString(6));
+		}
 		
 		
 	}
